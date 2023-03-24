5358ee3233bd5a5a503611632ac93498b1863903
fhros <112174015+fhros@users.noreply.github.com>
2023-03-24 09:28:03 +0200

    update L L LL

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,13 @@ let nume = prompt("anna numero NYT");
 
 let test = document.getElementById("numero");
 
-if(nume > 18) {
+if(nume > 1) {
     test.innerText = Number(nume);
+} 
+else if(nume <= 1) {
+    test.innerText ="oot tyhmä nyt loppuu";
 }
+else if(nume = Text) {
+    test.innerText = "OOT TYHMÄ LAITOIT KIRJAIMIA, LAITA NUMEROITA!!!!!!";
+} 
+
